8b287fe515b2bf453e9321d9e16475778d99449e
John Jensen MBP <johncjensen@hotmail.com>
2014-01-14 15:41:02 -0800

    Changed frontend Design to johncjensen.com and test to search for 'jensen'

--- test/features/static_home_page_test.rb
+++ test/features/static_home_page_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class StaticHomePageTest < Capybara::Rails::TestCase
   test "sanity" do
     visit root_path
-    assert_content page, "Hello Internets"
+    assert_content page, "Jensen"
     refute_content page, "Goobye All!"
   end
 end
