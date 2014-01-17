8f62165148478d60662e543d9db0be9cc96354a6
John Jensen MBP <johncjensen@hotmail.com>
2014-01-17 15:21:38 -0800

    Revert "fixing shit"
    This reverts commit 30d0e93b3c9a4a56c8dc7b5d4f299faf0fa92866.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,17 +20,11 @@ Portfolio::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
-  # Add the fonts path
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-
-  # Precompile additional assets
-  config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
