7a81f9619ddec340e63a343558eb7f43e61c4ec3
John Jensen MBP <johncjensen@hotmail.com>
2014-01-20 22:30:16 -0800

    added config.assets.compile = false to production env

--- Gemfile
+++ Gemfile
@@ -10,20 +10,19 @@ gem 'turbolinks', '2.2.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'compass-rails', '~> 1.1.3'
 
-group :development do
-  gem 'sqlite3', '1.3.8'
-end
-
 group :production do
   gem 'rails_12factor'
   gem 'pg','0.17.1'
+end
+
+group :development, :test do
+  gem 'sqlite3', '1.3.8'
 end
 
 group :test do
   gem 'turn'
 	gem 'minitest-rails-capybara', '0.10.0'
   gem 'launchy'
-  gem 'sqlite3', '1.3.8'
 end
 group :doc do
   gem 'sdoc', require: false
--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ module Portfolio
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     I18n.enforce_available_locales = false
-    config.assets.initialize_on_precompile = false
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
