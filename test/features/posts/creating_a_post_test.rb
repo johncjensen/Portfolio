dbcbdf9c1fe263ca40709fb6bddbb27d423fe3a3
John Jensen MBP <johncjensen@hotmail.com>
2014-01-27 14:13:56 -0800

    Added testing for authorization

--- test/features/posts/creating_a_post_test.rb
+++ test/features/posts/creating_a_post_test.rb
@@ -1,8 +1,36 @@
 require "test_helper"
 
 feature "Creating a post" do
-  scenario "submit form data to create a new post" do
 
+  scenario "unauthenticated site visitors cannot visit new_post_path" do
+    visit new_post_path
+    page.must_have_content "You need to sign in or sign up before continuing"
+  end
+
+  scenario "unauthenticated site vistiors cannot see new post button" do
+    visit posts_path
+    page.wont_have_link "New Post"
+  end
+
+  scenario "authors can't publish" do
+    sign_in(:author)
+    visit new_post_path
+    page.wont_have_field('published')
+  end
+
+  scenario "editors can publish" do
+    sign_in(:editor)
+    visit new_post_path
+    page.must_have_field('Published')
+    fill_in "Title", with: posts(:ac).title
+    fill_in "Body", with: posts(:ac).body
+    check "Published"
+    click_on "Create Post"
+    page.text.must_include "Status: Published"
+  end
+
+  scenario "submit form data to create a new post" do
+    sign_in(:author)
     visit new_user_session_path
     fill_in "Email", with: "batman@gotham.com"
     fill_in "Password", with: "grapplinghook89"
@@ -16,5 +44,6 @@ feature "Creating a post" do
     page.text.must_include posts(:ac).title
     page.has_css? ".blog-author"
     page.text.must_include "batman@gotham.com"
+    page.text.must_include "Status: Unpublished"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,10 @@ class ActionDispatch::IntegrationTest
   include Rails.application.routes.url_helpers
   include Capybara::DSL
 end
+
+def sign_in(role = :editor)
+  visit new_user_session_path
+  fill_in "Email", with: users(role).email
+  fill_in "Password", with: "password"
+  click_on "Sign in"
+end
