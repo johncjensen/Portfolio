d2f105459c13308361cfbc5c685dd7410901f39e
John Jensen MBP <johncjensen@hotmail.com>
2014-02-25 09:43:02 -0800

    move mandrill api key & username to figaro yml

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address => "smtp.mandrillapp.com",
   :port => 587,
   :domain => "johncjensen.com",
-  :user_name => "johncjensen@hotmail.com",
-  :password => "nQ2zQaLfxFMYwSrxxxbuag",
+  :user_name => ENV["MANDRILL_USERNAME"],
+  :password => ENV["MANDRILL_API_KEY"],
   :authentication => "plain",
   # :enable_starttls_auto => true
 }
